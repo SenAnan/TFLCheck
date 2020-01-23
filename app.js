45428c2498026564034a93fc3cacee1893759b5a
Sen Ananthasayanan <sen.sayanan@gmail.com>
2020-01-23 20:24:16 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let statuses = [];
+const statusTexts = Array.from(document.querySelectorAll('.status'));
+const statusBoxes = Array.from(document.querySelectorAll('.line__status-box'));
+const refreshButton = document.querySelector('.btn');
+
+function refresh() {
+	const getStatus = fetch('https://api.tfl.gov.uk/Line/Mode/tube/Status');
+	getStatus
+		.then(data => {
+			return data.json();
+		})
+		.then(json => {
+			json.forEach(el => {
+				statuses.push(el.lineStatuses[0].statusSeverityDescription);
+			});
+		})
+		.then(() => {
+			for (let i = 0; i < 11; i++) {
+				statusTexts[i].innerHTML = statuses[i];
+				if (statuses[i] == 'Good Service') {
+					statusBoxes[i].classList.add('status__good');
+				} else if (statuses[i] == 'Minor Delays') {
+					statusBoxes[i].classList.add('status__minor');
+				} else if (statuses[i] == 'Severe Delays') {
+					statusBoxes[i].classList.add('status__severe');
+				} else if (statuses[i] == 'Closed' || statuses[i] == 'No Service') {
+					statusBoxes[i].classList.add('status__closed');
+				} else if (statuses[i] == 'Planned Closure' || statuses[i] == 'Part Closure') {
+					statusBoxes[i].classList.add('status__part');
+				}
+			}
+		});
+}
+
+refreshButton.addEventListener('click', () => {
+	refresh();
+	console.log('refreshed');
+});
+
+refresh();
